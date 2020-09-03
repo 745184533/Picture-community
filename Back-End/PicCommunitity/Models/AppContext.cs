839f1b5dc66d19986b9cdedefaa8119b36edc7d1
Dark Souls <745184533@qq.com>
2020-09-03 10:48:35 +0800

    实验。

---
+++ Back-End/PicCommunitity/Models/AppContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicCommunitity.Models
+{
+    public class AppContext:DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           // modelBuilder.Entity<buyhistory>().HasKey(t => new { t.u_id, t.E_id, t.buy_time });
+
+            //modelBuilder.Entity<shopcart>().HasKey(t => new { t.u_id, t.E_id });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}

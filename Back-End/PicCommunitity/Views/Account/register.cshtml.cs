7c4b4746370606a00a7308ad2b8aa3333b5fe4fe
Dark Souls <745184533@qq.com>
2020-09-07 19:25:42 +0800

    初步把杨世吉同学的文件添加到项目中

---
+++ Back-End/PicCommunitity/Views/Account/register.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PicCommunitity.Views.Account
+{
+    public class registerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

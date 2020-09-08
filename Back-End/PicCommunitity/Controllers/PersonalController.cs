936672b193fbf58cd76f8e66098f5ff7e0dabc25
Dark Souls <745184533@qq.com>
2020-09-08 09:20:28 +0800

    暂填王佳铭同学的页面

---
+++ Back-End/PicCommunitity/Controllers/PersonalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PicCommunitity.Controllers
+{
+    public class PersonalController : Controller
+    {
+        [Route("/Personal/Picture")]
+        public IActionResult Picture()
+        {
+            return View();
+        }
+    }
+}
---
+++ Back-End/PicCommunitity/Views/Personal/Picture.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PicCommunitity.Views.Personal
+{
+    public class PersonalPictureModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

711e353f67fed629ebc3a606e20ea606180d1a86
Amonkey00 <892542697@qq.com>
2020-09-09 21:54:34 +0800

    恢复MVC架构，完成登陆注册界面以及Cookies的Authentication
    rt

--- Back-End/PicCommunitity/Controllers/HomeController.cs
+++ Back-End/PicCommunitity/Controllers/HomeController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PicCommunitity.Models;
 
 namespace PicCommunitity.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class HomeController : Controller
     {
 
@@ -17,9 +18,15 @@ namespace PicCommunitity.Controllers
         }
         [Route("Index")]
         [HttpGet]
-        public blog Index()
+        public IActionResult Index()
         {
-            return null;
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                ViewBag.UserName = HttpContext.User.Claims.First().Value;
+                ViewBag.logined = true;
+            }
+            else ViewBag.logined = false;
+            return View();
         }
     }
 }

c9835625a8e398e1f67b2af685e45b274b495151
Dark Souls <745184533@qq.com>
2020-09-16 00:47:51 +0800

    错了

---
+++ Back-End/PicCommunitity/ViewsModels/OtherComment.cs
@@ -0,0 +1,15 @@
+﻿using PicCommunitity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicCommunitity.ViewsModels
+{
+    public class OtherComment
+    {
+        public string userId { get; set; }
+        public string userName { get; set; }
+        public picComment content { get; set; }
+    }
+}

ccbb657487d4da707d60822dffb2196b8676d8e8
kuronekowww <49890629+kuronekowww@users.noreply.github.com>
2020-09-18 18:56:42 +0800

    更新
    动态图片显示 动态更新点赞数等数据

--- Before-End/Picture-Community-Before/js/个人图片主页.js
+++ Before-End/Picture-Community-Before/js/个人图片主页.js
@@ -1,37 +1,84 @@
 // JavaScript Document$(function(){
-state.onclick = function()
-                {
-                    var text = this.innerHTML;
-                    if(text == "-")
-                    {
-                        this.innerHTML = "+";
-                        uploadpic.style.display = "none";
-                    }
-                    else
-                    {
-                        this.innerHTML = "-";
-                        uploadpic.style.display = "block";
-                    }
-                }
- var $ = function(ids)
-                {
-                    return document.getElementById(ids);
-                }
+localStorage.setItem("picId","1");
+localStorage.setItem("userId","2");
 
-                var tabs = $("tabs").getElementsByTagName("li");
-                var state = $("state");
-                var uploadpic = $("uploadpic");
-                var ul = uploadpic.getElementsByTagName("ul");
-for(var i = 0; i < tabs.length; i++)
-                {
-                    tabs[i].index = i;
-                    tabs[i].onmouseover = function()
-                    {
-                        for(var i = 0; i < tabs.length; i++)
-                        {
-                            tabs[i].className = ul[i].className = "";
-                        }
-                        this.className = ul[this.index].className = "active";
-                    `
 
-                }
+//var source=localStorage.getItem("scr");
+var picId=localStorage.getItem("picId");
+var userId=localStorage.getItem("userId");
+var settings = {
+  "url": "http://172.81.239.44/Account/getUserInfo?userId=2",
+  "method": "GET",
+  "timeout": 0,
+  "headers":{"Authorization":"Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NTRjZGZmOS03ODQ2LTRjYzAtOWNjMy01ZjhkOTJjMmY5NjAiLCJuYW1lIjoicm9vdCIsIm5iZiI6MTYwMDQxOTI1MiwiZXhwIjoxNjAwNDE5ODUyLCJpc3MiOiJqd3RJc3N1ZXJ0ZXN0IiwiYXVkIjoiand0QXVkaWVuY2V0ZXN0In0.e7tPDpt2RFxEsqL_cLsp8K32JJbQSAY-RGAeuw95R9w"},
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+	console.log(response.userName);
+	$("#user").append("<h3>"+response.userName+"</h3>");
+});
+
+var settings = {
+  "url": "http://172.81.239.44/Account/getProfileInfo?userId=2",
+  "method": "GET",
+  "timeout": 0,
+  "headers":{"Authorization":"Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NTRjZGZmOS03ODQ2LTRjYzAtOWNjMy01ZjhkOTJjMmY5NjAiLCJuYW1lIjoicm9vdCIsIm5iZiI6MTYwMDQxOTI1MiwiZXhwIjoxNjAwNDE5ODUyLCJpc3MiOiJqd3RJc3N1ZXJ0ZXN0IiwiYXVkIjoiand0QXVkaWVuY2V0ZXN0In0.e7tPDpt2RFxEsqL_cLsp8K32JJbQSAY-RGAeuw95R9w"},
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+	console.log(response.starNum);
+	$("#icon1").append("<span>"+response.starNum+"</span>");
+	$("#icon2").append("<span>"+response.followNum+"</span>");
+	$("#icon3").append("<span>"+response.likeNum+"</span>");
+	$("#icon4").append("<span>"+response.commentNum+"</span>");
+});
+
+	var settings = {
+  "url": "http://172.81.239.44/Account/getProfilePicture?userId=1",
+  "method": "GET",
+  "timeout": 0,
+  "headers":{"Authorization":"Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NTRjZGZmOS03ODQ2LTRjYzAtOWNjMy01ZjhkOTJjMmY5NjAiLCJuYW1lIjoicm9vdCIsIm5iZiI6MTYwMDQxOTI1MiwiZXhwIjoxNjAwNDE5ODUyLCJpc3MiOiJqd3RJc3N1ZXJ0ZXN0IiwiYXVkIjoiand0QXVkaWVuY2V0ZXN0In0.e7tPDpt2RFxEsqL_cLsp8K32JJbQSAY-RGAeuw95R9w"},
+};
+$.ajax(settings).done(function(response) {
+  console.log(response);
+	var arr=response.upload;
+	var container=document.querySelector("#page1");
+	arr.forEach(function(item,index){
+		var div=document.createElement('div');
+		div.setAttribute("class","thumbnail");
+		var img=document.createElement('img');
+		var pictitle=document.createElement('h4');
+		pictitle.setAttribute("title",item.thatpicture.p_info);
+		//pictitle.value=item.thatpicture.p_info;
+		console.log(item.thatpicture.p_info);
+		img.src=item.thatpicture.p_url;
+		img.setAttribute("class","cards");
+		div.appendChild(img);
+		div.appendChild(pictitle);
+		container.appendChild(div);
+})
+});
+
+	var settings = {
+  "url": "http://172.81.239.44/Account/getProfilePicture?userId=1",
+  "method": "GET",
+  "timeout": 0,
+  "headers":{"Authorization":"Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NTRjZGZmOS03ODQ2LTRjYzAtOWNjMy01ZjhkOTJjMmY5NjAiLCJuYW1lIjoicm9vdCIsIm5iZiI6MTYwMDQxOTI1MiwiZXhwIjoxNjAwNDE5ODUyLCJpc3MiOiJqd3RJc3N1ZXJ0ZXN0IiwiYXVkIjoiand0QXVkaWVuY2V0ZXN0In0.e7tPDpt2RFxEsqL_cLsp8K32JJbQSAY-RGAeuw95R9w"},
+};
+$.ajax(settings).done(function(response) {
+  console.log(response);
+	var arr=response.upload;
+	var container=document.querySelector("#page2");
+	arr.forEach(function(item,index){
+			var div=document.createElement('div');
+			div.setAttribute("class","thumbnail");
+			var img=document.createElement('img');
+			img.src=item.thatpicture.p_url;
+			img.setAttribute("class","cards");
+			div.appendChild(img);
+			container.appendChild(div);
+})
+});
+
